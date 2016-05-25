02150b7b64580191db03a6b62d230233b285ea41
MONTES Yannick <montes.yannick@gmail.com>
2016-05-25 19:29:43 +0200

    Added the ShotScript.

---
+++ Assets/Scripts/ShotScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotScript : MonoBehaviour {
+
+	/// <summary>
+	/// Inflected damages.
+	/// </summary>
+	public int damages = 1;
+
+	/// <summary>
+	/// Is it a shot from the player or not?
+	/// </summary>
+	public bool isEnnemyShot = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//We limit the time to live of a shot to 20 seconds.
+		Destroy (gameObject, 20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
