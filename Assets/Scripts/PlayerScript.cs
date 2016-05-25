96a6821f52b3296328b8d5ee59e6c3abcbae088f
MONTES Yannick <montes.yannick@gmail.com>
2016-05-25 18:56:30 +0200

    Added the basic gameplay: We can moove the player. Split the project into folder, Prefabs, Scenes, Scripts, Sounds and Textures.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	/// <summary>
+	/// Variable for the speed
+	/// </summary>
+	public Vector2 speed = new Vector2 (50, 50);
+
+	/// <summary>
+	/// Variable for the movement.
+	/// </summary>
+	private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Here we get the information about what the player done.
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+		//We affect the movement in function of speed.
+		movement = new Vector2 (speed.x * inputX, speed.y * inputY);
+	}
+
+	void FixedUpdate()
+	{
+		//Here we affect the movement to the object.
+		GetComponent<Rigidbody2D>().velocity = movement;
+	}
+}
